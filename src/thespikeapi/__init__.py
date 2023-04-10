0b82906aa148363c1a5f11c13a8b14634c37d823
Nick Torstenson <20603772+NickTorstenson@users.noreply.github.com>
2023-04-09 23:44:56 -0500

    unused __init__ stuff

--- src/thespikeapi/__init__.py
+++ src/thespikeapi/__init__.py
@@ -1,8 +1,5 @@
-#from .thespikeapi import get_matches_by_status
+
 from .thespikeapi import get_match_by_id
-#from .thespikeapi import get_news 
-from .thespikeapi import get_player_matches_by_id
-#from .thespikeapi import get_top_n
 from .thespikeapi import get_player_infos
 from .thespikeapi import get_player_match_ids
 from .thespikeapi import to_json
