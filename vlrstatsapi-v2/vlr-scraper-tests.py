bdd96f4525e5c08da5a6ceea94b32f511fef73da
Nick Torstenson <20603772+NickTorstenson@users.noreply.github.com>
2023-05-01 22:34:00 +0000

    test changes

--- vlrstatsapi-v2/vlr-scraper-tests.py
+++ vlrstatsapi-v2/vlr-scraper-tests.py
@@ -54,13 +54,15 @@ matches = []
 for team in all_regions:
     matches += vlrs.get_team_match_ids(team, 50)
     unique_matches = list(set(matches))
-    #print(len(unique_matches))
+    print(len(unique_matches))
+
+#print(vlrs.get_player_adrs(vlrs.get_game_soups(183804)[0]))
 
 #vlrs.to_csv(data, f'match_soups_storage({today})(1)')
 #vlrs.get_match_player_data([64566])[1]
 
-# #, soups_file='match_soups_storage(2023-04-24).csv'
-# data = vlrs.get_match_player_data(unique_matches, soups_file='match_soups_storage(2023-04-24).csv')
-# storage = pd.DataFrame(data[1])
-# vlrs.to_csv(data[0], f'playerdata({today})(Last50All)(1)')
-# vlrs.to_csv(storage, f'match_soups_storage({today})(1).csv')
+#, soups_file='match_soups_storage(2023-04-24).csv'
+data = vlrs.get_match_player_data(unique_matches, soups_file='match_soups_storage(2023-04-24).csv')
+storage = pd.DataFrame(data[1])
+vlrs.to_csv(data[0], filename=f'playerdata({today})(Last50All)(1)')
+vlrs.to_csv(storage, filename=f'match_soups_storage({today})(1).csv')
